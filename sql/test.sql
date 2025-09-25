1b4f55a582f6e5774f93a104714a03097af267f3
Jorge Fernandez <98422434+jfernandezatpythian@users.noreply.github.com>
2025-09-25 13:34:32 -0600

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -8,10 +8,10 @@ CREATE OR REPLACE PACKAGE emp_pkg2 AS
    * @param p_salary      The new employee's monthly salary.
    */
   PROCEDURE hire_employee (
-      p_employee_id   IN  employees.employee_id%TYPE,
-      p_first_name    IN  employees.first_name%TYPE,
-      p_job_title     IN  employees.job_title%TYPE,
-      p_salary        IN  employees.salary%TYPE
+      p_employee_id   IN  system.employees.employee_id%TYPE,
+      p_first_name    IN  system.employees.first_name%TYPE,
+      p_job_title     IN  system.employees.job_title%TYPE,
+      p_salary        IN  system.employees.salary%TYPE
   );
 
   /**
@@ -20,7 +20,7 @@ CREATE OR REPLACE PACKAGE emp_pkg2 AS
    * @return The calculated annual salary (salary * 12).
    */
   FUNCTION get_annual_salary (
-      p_employee_id   IN  employees.employee_id%TYPE
+      p_employee_id   IN  system.employees.employee_id%TYPE
   ) RETURN NUMBER;
 
 END emp_pkg2;
