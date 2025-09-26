ffa7601a9300fd632d912d36466b2e510c60fa5c
Jorge Fernandez <98422434+jfernandezatpythian@users.noreply.github.com>
2025-09-26 10:29:47 -0600

    Update 001_create_employees.sql

--- sql/001_create_employees.sql
+++ sql/001_create_employees.sql
@@ -1,14 +1,14 @@
 --liquibase formatted sql
 
 --changeset jdf:1
-CREATE TABLE employees (
+CREATE TABLE employees2 (
     employee_id NUMBER PRIMARY KEY,
     first_name VARCHAR2(50),
     job_title VARCHAR2(50)
 );
 
 --changeset yourname:2
-CREATE VIEW vw_dummy_data AS
+CREATE VIEW vw_dummy_data2 AS
 SELECT 
     'Hello World' AS message,
     123           AS sample_number,
