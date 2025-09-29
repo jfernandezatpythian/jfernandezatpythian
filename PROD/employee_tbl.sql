b022683384a33123f232c76e7459eb8385f051e7
Jorge Fernandez <98422434+jfernandezatpythian@users.noreply.github.com>
2025-09-29 16:46:59 -0600

    Update employee_tbl.sql
    Update on table

--- PROD/employee_tbl.sql
+++ PROD/employee_tbl.sql
@@ -1,11 +1,11 @@
-CREATE TABLE employees4 (
+CREATE TABLE employees5 (
     employee_id NUMBER PRIMARY KEY,
     first_name VARCHAR2(50),
     job_title VARCHAR2(50)
 );
 
 --changeset yourname:2
-CREATE VIEW vw_dummy_data4 AS
+CREATE VIEW vw_dummy_data5 AS
 SELECT 
     'Hello World' AS message,
     123           AS sample_number,
